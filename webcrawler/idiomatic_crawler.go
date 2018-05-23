0c5a57f387e21aff033ae0f2a144ebd591aa096b
cezkuj <cezkuj@gmail.com>
2018-05-23 14:27:52 +0000

    Unleaking goroutines

--- webcrawler/idiomatic_crawler.go
+++ webcrawler/idiomatic_crawler.go
@@ -35,14 +35,14 @@ func NewIdiomaticCrawler(domain string, matchSubdomains bool, reqInterval time.D
 func (crawler IdiomaticCrawler) Crawl() sync.Map {
 	crawler.wg.Add(1)
 	go func() {
-		for {
+		for page := range(crawler.toFetch){
 			time.Sleep(crawler.reqInterval)
-			go crawler.fetch(<-crawler.toFetch)
+			go crawler.fetch(page)
 		}
 	}()
 	go func() {
-		for {
-			go crawler.scrap(<-crawler.toScrap)
+		for page := range(crawler.toScrap) {
+			go crawler.scrap(page)
 		}
 	}()
 	prot := "https"
@@ -52,6 +52,8 @@ func (crawler IdiomaticCrawler) Crawl() sync.Map {
 	mainPage := prot + "://" + crawler.domain
 	crawler.toFetch <- (mainPage)
 	crawler.wg.Wait()
+        close(crawler.toFetch)
+        close(crawler.toScrap)
 	//Avoid infinite loops in printing by deleting main page
 	crawler.visitedPages.Delete(mainPage)
 	return *crawler.visitedPages
@@ -64,7 +66,7 @@ func (crawler IdiomaticCrawler) fetch(page string) {
 		log.Println(err)
 		return
 	}
-	crawler.wg.Add(1)
+        crawler.wg.Add(1)
 	crawler.toScrap <- Page{name: page, content: doc}
 }
 
@@ -72,13 +74,13 @@ func (crawler IdiomaticCrawler) scrap(page Page) {
 	defer crawler.wg.Done()
 	if link, found := findLink(page); found {
 		if u, inserted := insertURL(link, page.name, crawler.domain, crawler.matchSubdomains, crawler.visitedPages); inserted {
-			crawler.wg.Add(1)
+                        crawler.wg.Add(1)
 			crawler.toFetch <- u
 		}
 
 	}
 	for child := page.content.FirstChild; child != nil; child = child.NextSibling {
-		crawler.wg.Add(1)
+                crawler.wg.Add(1)
 		crawler.toScrap <- Page{name: page.name, content: child}
 	}
 }
